7e558e9e709bdef6aa140ad593a2555a3e7d3e36
bhushanpoojary <64708832+bhushanpoojary@users.noreply.github.com>
2025-05-05 00:15:37 +0530

    Added concept of thread code

---
+++ Code/2 Fundamentals of thread in .NET/1 Thread/ThreadConcept/ThreadConcept/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ThreadConcept
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(OpenDoor);
+            thread.Name = "Worker-1";
+            thread.Name = "Worker-2";
+            thread.Start();
+            thread.Join();
+            BoilingWater();
+        }
+        private static void BoilingWater()
+        {
+            Console.WriteLine("Boiling water");
+        }
+        private static void OpenDoor()
+        {
+            Thread.Sleep(5000);
+            Console.WriteLine("Open Door");
+        }
+    }
+}
