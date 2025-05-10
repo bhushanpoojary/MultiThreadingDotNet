f94abc27a216073e46dcd8e8f4a3b2876b2f2658
bhushanpoojary <64708832+bhushanpoojary@users.noreply.github.com>
2025-05-10 16:18:19 +0530

    Pushing thread changes

---
+++ Code/3 Fundamentals of Threads/BackgroundForegroundThread/BackgroundForegroundThread/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace BackgroundForegroundThread
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(() => {
+                while (true)
+                {
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Heartbeat");
+                }
+            });
+            t.IsBackground = true;
+            t.Start();
+            Thread.Sleep(5000);
+        }
+    }
+}
---
+++ Code/3 Fundamentals of Threads/ThreadStartDelegate/ThreadStartDelegate/Program.cs
@@ -0,0 +1,38 @@
+﻿
+namespace ThreadStartDelegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //This works because of method group conversion,
+            //where the compiler infers and converts
+            //Print to a ThreadStart delegate automatically
+            //if the method signature matches.
+            Thread thread = new Thread(Print);
+            thread.Name = "Worker";
+            Console.WriteLine(thread.ThreadState);
+            Console.WriteLine(thread.IsAlive);
+            thread.Start();
+            Console.WriteLine(thread.IsAlive);
+            Console.WriteLine(thread.ThreadState);
+            thread.Join();
+            Console.WriteLine(thread.ThreadState);
+            Console.WriteLine(thread.IsAlive);
+            Thread thread2 = new Thread(new ThreadStart(Print));
+            thread2.Name = "Worker-2";
+            thread2.Start();
+            Thread thread3 = new Thread(new ParameterizedThreadStart(PrintMessage));
+            thread3.Start("Hello world!");
+        }
+
+        private static void Print()
+        {
+            Console.WriteLine("Hello from " + Thread.CurrentThread.Name);
+        }
+        static void PrintMessage(object message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
