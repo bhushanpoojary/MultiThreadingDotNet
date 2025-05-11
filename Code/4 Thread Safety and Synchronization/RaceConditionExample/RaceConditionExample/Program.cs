4f779b3ede772ad634739e83c2b13c081a03ed66
bhushanpoojary <64708832+bhushanpoojary@users.noreply.github.com>
2025-05-11 22:07:39 +0530

    Added Race Condition

---
+++ Code/4 Thread Safety and Synchronization/RaceConditionExample/RaceConditionExample/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace RaceConditionExample
+{
+    internal class Program
+    {
+        static int counter = 0;
+
+        static void Increment()
+        {
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                counter= counter + 1; // Not thread-safe!
+            }
+        }
+
+        static void Main()
+        {
+            Thread t1 = new Thread(Increment);
+            Thread t2 = new Thread(Increment);
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+            Console.WriteLine($"Final Counter Value: {counter}");
+        }
+    }
+}
