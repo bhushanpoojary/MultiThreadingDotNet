0f4e06fc30f30ac6b74935c11f8db950ca946bf1
bhushanpoojary <64708832+bhushanpoojary@users.noreply.github.com>
2025-05-12 09:24:24 +0530

    Added lock code

---
+++ Code/4 Thread Safety and Synchronization/LockRaceCondition/LockRaceCondition/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace LockRaceCondition
+{
+    internal class Program
+    {
+        static int counter = 0;
+        static readonly object _syncObj = new object();
+
+        static void Increment()
+        {
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                lock (_syncObj)
+                {
+                    counter = counter + 1; 
+                }
+            }
+        }
+
+        static void Main()
+        {
+            Thread t1 = new Thread(Increment);
+            Thread t2 = new Thread(Increment);
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+            Console.WriteLine($"Final Counter Value: {counter}");
+        }
+    }
+}
