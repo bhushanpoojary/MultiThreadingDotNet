1121cab77833f420a80245af3ef3a47218f023c4
bhushanpoojary <64708832+bhushanpoojary@users.noreply.github.com>
2025-05-13 23:42:00 +0530

    Added Monitor concept

---
+++ Code/4 Thread Safety and Synchronization/3 Monitor/MonitorExample/MonitorExample/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace MonitorExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter = new Counter();
+            counter.Increment();
+            Console.WriteLine(counter.GetCount().ToString());
+        }
+    }
+    class Counter
+    {
+        private int _count = 0;
+        private readonly object _lock = new object();
+
+        public void Increment()
+        {
+            try
+            {
+                Monitor.Enter(_lock);// Acquires monitor lock
+                {
+                    _count++;
+                } // Releases lock automatically here
+            }
+            finally
+            {
+                Monitor.Exit(_lock);
+            }
+            
+        }
+
+        public int GetCount()
+        {
+            lock (_lock)
+            {
+                return _count;
+            }
+        }
+
+        public void Decrement()
+        {
+            if (Monitor.TryEnter(_lock, TimeSpan.FromSeconds(2)))
+            {
+                try
+                {
+                    _count--;
+                }
+                finally
+                {
+                    Monitor.Exit(_lock);
+                }
+            }
+            else
+            {
+                // Handle timeout (e.g., log, retry)
+            }
+        }
+    }
+}
