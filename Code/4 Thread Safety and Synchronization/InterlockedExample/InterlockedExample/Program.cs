a2b7e607c1c8598fa328bf6989ca9ae21c9bb74a
bhushanpoojary <64708832+bhushanpoojary@users.noreply.github.com>
2025-05-16 19:17:20 +0530

    Added Interlocked example

---
+++ Code/4 Thread Safety and Synchronization/InterlockedExample/InterlockedExample/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace InterlockedExample
+{
+    class Program
+    {
+        static int counter = 0;
+
+        static void IncrementUnsafe()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                counter++; // Not thread-safe
+            }
+        }
+
+        static void IncrementSafe()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                Interlocked.Increment(ref counter); // Thread-safe
+            }
+        }
+
+        static void Main()
+        {
+            Thread t1 = new Thread(IncrementSafe);
+            Thread t2 = new Thread(IncrementSafe);
+
+            t1.Start();
+            t2.Start();
+            t1.Join();
+            t2.Join();
+
+            Console.WriteLine("Counter: " + counter); // Should be 20000
+        }
+    }
+}
