8cf273edee660bfaa05e56b099651adf1f984bf5
bhushanpoojary <64708832+bhushanpoojary@users.noreply.github.com>
2025-05-18 15:00:23 +0530

    Added example of timer

---
+++ Code/5 Advanced Threading Concepts/TimerExample/TimerExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static Timer _timer;
+
+    static void Main()
+    {
+        // Timer callback every 2 seconds
+        _timer = new Timer(DoWork, null, 0, 2000);
+
+        Console.WriteLine("Timer started. Press any key to exit.");
+        Console.ReadKey();
+    }
+
+    static void DoWork(object state)
+    {
+        Console.WriteLine($"Work done at: {DateTime.Now}");
+    }
+}
