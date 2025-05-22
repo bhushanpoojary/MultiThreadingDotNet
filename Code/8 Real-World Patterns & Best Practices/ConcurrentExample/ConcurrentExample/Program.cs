45c84a782116b552b73c56fc3cfa55daf676c80a
bhushanpoojary <64708832+bhushanpoojary@users.noreply.github.com>
2025-05-22 07:47:25 +0530

    Added Concurrent collection

---
+++ Code/8 Real-World Patterns & Best Practices/ConcurrentExample/ConcurrentExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Concurrent;
+
+namespace ConcurrentExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcurrentDictionary<int, string> users = new();
+            users.TryAdd(1, "Alice");
+            string user = users.GetOrAdd(2, id => "Bob");
+            Console.WriteLine(user);
+            user = users.GetOrAdd(2, id => "Marwin");
+            Console.WriteLine(user);
+            ConcurrentQueue<string> messages = new();
+            messages.Enqueue("Hello");
+            if (messages.TryDequeue(out var msg))
+                Console.WriteLine(msg); // "Hello"
+        }
+    }
+}
