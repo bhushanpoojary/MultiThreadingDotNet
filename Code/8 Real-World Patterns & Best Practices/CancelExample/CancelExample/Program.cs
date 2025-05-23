b67b041bc41351c110601e0ca378f1510d1b7134
bhushanpoojary <64708832+bhushanpoojary@users.noreply.github.com>
2025-05-23 23:57:52 +0530

    Added Cancellation token source

---
+++ Code/8 Real-World Patterns & Best Practices/CancelExample/CancelExample/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace CancelExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken token = cts.Token;
+
+            Task task = Task.Run(() =>
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    if (token.IsCancellationRequested)
+                    {
+                        Console.WriteLine("Cancellation requested!");
+                        token.ThrowIfCancellationRequested();
+                        return; // Exit cleanly
+                    }
+
+                    Console.WriteLine("Working... " + i);
+                    Thread.Sleep(100);
+                }
+            }, token);
+
+            // Simulate cancellation after 1 second
+            Thread.Sleep(1000);
+            cts.Cancel();
+            task.Wait();
+        }
+    }
+}
